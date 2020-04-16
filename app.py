0563e72cdeb4994d2a04709c49cdd65acf6f9260
irao413 <59344623+irao413@users.noreply.github.com>
2020-04-15 17:41:40 -0700

    flask complete

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 # Flask Routes
 #################################################
 @app.route("/")
-def home_page():
+    def home_page():
     <!DOCTYPE HTML>
     <html lng="en=us">
     <head>
@@ -42,7 +42,7 @@ def home_page():
 </html>
 
 @app.route("/api/v1.0/precipitation")
-def precipitation():
+    def precipitation():
     """Precipitation Records"""
 
     session = Session(engine)
@@ -61,8 +61,8 @@ def precipitation():
     return jsonify(dict)
 
 
-    @app.route("/api/v1.0/stations")
-def stations():
+ @app.route("/api/v1.0/stations")
+    def stations():
     """Station Records"""
 
     session = Session(engine)
@@ -75,3 +75,55 @@ def stations():
     station_list = [list(U) for U in stations]
 
     return jsonify(station_list)
+
+@app.route("/api/v1.0/tobs")
+    def tobs():
+    "Temperatures"
+
+    session = Session(engine)
+
+    stations = session.query(Measurement.station, func.count(Measurement.station)).\
+        group_by(Measurement.station).order_by(func.count(Measurement.station).desc()).all()
+
+    one_year = dt.date(2017, 8, 9) - dt.timedelta(days=365)
+
+    tobs = session.query(Measurement.date, Measurement.tobs).filter(Measurement.date > one_year).all()
+    
+    session.close()
+
+    tobs_list = [list(t) for t in tobs]
+
+    return jsonify(tobs_list)
+
+@app.route("/api/v1.0/<1/1/2010>")
+    def start():
+        """Start Date"""
+
+        session = Session(engine)
+
+        start_session = session.query(Measurement.date, func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).\
+        filter(Measurement.date >= start_date).group_by(Measurement.date).all()
+
+        session.close()
+
+        start_list = [list(s) for s in start]
+
+        return jsonify(start_list)
+
+@app.route(/api/v1.0/1/1/2010/<8/9/2017>)
+    def end():
+        "End Date"
+
+        session = Session(engine)
+
+        end_session = session.query(Measurement.date, func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).\
+        filter(Measurement.date >= end_date).group_by(Measurement.date).all()
+
+        session.close()
+
+        end_list = [list(e) for e in end]
+
+        return jsonify(end_list)
+
+if __name__ == "__main__":
+    app.run(debug=True)
